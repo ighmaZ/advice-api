59253d56bad7935db5538b826dc1a745468d4f0f
ighmaz <ighmazcool@gmail.com>
2022-09-19 19:17:53 +0530

    fetched

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from './components/Card'
 
 const App = () => {
+  
   return (
     <div className='container'>
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import {BsFillDice5Fill} from 'react-icons/Bs'
 
 const Card = () => {
 
+    const[advice,setAdvice] = useState("");
+    const[id, setId] = useState(0);
+
 useEffect(()=>{
 
-    fetch('https://api.adviceslip.com/advice')
-    .then
+    fetch( 'https://api.adviceslip.com/advice')
+    .then((response) => response.json())
+    .then((data) => {
+        
+        setAdvice(data.slip.advice)
+        setId(data.slip.id)
+    } )
+        
+    .catch((err)=> console.log(err.message))
+    
+
 
 },[]);
 
   return (
     <>
     <div className='card'>
-    <h6>ADVICE</h6>
-    <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, voluptas!"</p>
+    <h6>ADVICE #{id}</h6>
+    <p>{advice}</p>
     <hr/>
     <button><BsFillDice5Fill/></button>
 
